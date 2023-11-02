85bed317431790862c8d4c8f8692470dc41b9cb3
bighwang <gxd0214@gmail.com>
2023-11-02 12:55:11 +0900

    configuration & bean annotation

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
 import hello.core.discount.DiscountPolicy;
@@ -9,14 +12,17 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
-
-
+// Application에 설정 정보라는 뜻이다. Spring @Configuration
+@Configuration
 public class AppConfig {
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
     // MemberRepository에 대한 설정을 MemberServiceImpl에서 직접 했지만
     // 이제는 AppConfig에서 설정을 정리해준다.
-    public MemberService MemberService() {
+    // @Bean을 사용하면 Spring Container에 등록이 된다.
+    // @Bean(name="mms")   // name으로 getBean에서 불러오는 이름을 바꿀 수 있다. 하지만 default를 따르는 것이 좋다.
+    @Bean
+    public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
@@ -25,15 +31,18 @@ public class AppConfig {
     // }
     // 위 코드 리팩토링 - 메소드로 만들어 줌으로써, 향후에 계흭이 바뀌면 바꿔줄 수 있다.
     // 아래 OrderService에도 넣어줄 수 있다.
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 생성자 주입
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         // 할인 정책 변경
--- core/src/main/java/hello/core/OderApp.java
+++ core/src/main/java/hello/core/OderApp.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.Order.Order;
 import hello.core.Order.OrderService;
 import hello.core.member.Grade;
@@ -8,12 +11,29 @@ import hello.core.member.MemberService;
 
 public class OderApp {
     public static void main(String[] args) {
+        /*
         // MemberService memberService = new MemberServiceImpl();
         // OrderService orderService = new OrderServiceImpl();
         AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.MemberService();
+        MemberService memberService = appConfig.memberService();
         OrderService orderService = appConfig.orderService();
+        
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        
+        Order order = orderService.creatOrder(memberId, "itemA", 20000);
+        
+        System.out.println("order = " + order);
+        System.out.println("order.caculatePrice = " + order.calculatePrice());
+        
+        System.out.println("discount : " + order.getDiscountPrice());
+         */
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
